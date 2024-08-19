41df3258275140f33d4faaecd1f57c7ef69b3bb6
github-action-benchmark <github@users.noreply.github.com>
2024-08-19 16:04:23 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for eb97c540b67c7c109ffb29df3961a5b03f36ad6f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724081021960,
+  "lastUpdate": 1724083463753,
   "repoUrl": "https://github.com/SteveDunn/Intellenum",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 9.414976226290067,
             "unit": "ns",
             "range": "± 0.07667682005278216"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "steve@dunnhq.com",
+            "name": "Steve Dunn",
+            "username": "SteveDunn"
+          },
+          "committer": {
+            "email": "steve@dunnhq.com",
+            "name": "Steve Dunn",
+            "username": "SteveDunn"
+          },
+          "distinct": true,
+          "id": "eb97c540b67c7c109ffb29df3961a5b03f36ad6f",
+          "message": "Remove generated files",
+          "timestamp": "2024-08-19T16:21:01+01:00",
+          "tree_id": "38caf7f92e59dee2731de64c61261d93f04a0a59",
+          "url": "https://github.com/SteveDunn/Intellenum/commit/eb97c540b67c7c109ffb29df3961a5b03f36ad6f"
+        },
+        "date": 1724083463322,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FromNameBenchmarks.EnumGenerators_Try",
+            "value": 13.772456202904383,
+            "unit": "ns",
+            "range": "± 0.15143432597708392"
+          },
+          {
+            "name": "FromNameBenchmarks.Intellenums_Try",
+            "value": 9.418464971440178,
+            "unit": "ns",
+            "range": "± 0.08339949840068253"
           }
         ]
       }
